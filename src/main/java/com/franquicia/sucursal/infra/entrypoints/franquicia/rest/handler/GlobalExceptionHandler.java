18d1bbf37eb82592f791b27c6b000aa3f07056d0
BorisMejia <borisfernandez916@gmail.com>
2025-05-20 12:45:10 -0500

    feat: se implementan 3 nuevos manejadores de errores

--- src/main/java/com/franquicia/sucursal/infra/entrypoints/franquicia/rest/handler/GlobalExceptionHandler.java
+++ src/main/java/com/franquicia/sucursal/infra/entrypoints/franquicia/rest/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /*Manejo de errores de validaciones*/
     @ExceptionHandler(WebExchangeBindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Mono<Map<String, Object>> handleValidationException(WebExchangeBindException ex) {
@@ -26,6 +28,42 @@ public class GlobalExceptionHandler {
         error.put("mensajes", ex.getFieldErrors().stream()
                 .map(err -> err.getField() + ": " + err.getDefaultMessage())
                 .collect(Collectors.toList()));
+
+        return Mono.just(error);
+    }
+
+    /*Manejo de errores datos de entrada*/
+    @ExceptionHandler(ServerWebInputException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Mono<Map<String, Object>> handleWebInputException(ServerWebInputException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", HttpStatus.BAD_REQUEST.value());
+        error.put("error", "Entrada inválida");
+        error.put("mensaje", ex.getReason());
+
+        return Mono.just(error);
+    }
+
+    /*Manejo de errores Refglas de negocio*/
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Mono<Map<String, Object>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", HttpStatus.BAD_REQUEST.value());
+        error.put("error", "Regla de negocio violada");
+        error.put("mensaje", ex.getMessage());
+
+        return Mono.just(error);
+    }
+
+    /*Manejo de errores inesperados*/
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Mono<Map<String, Object>> handleOtherErrors(Throwable ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.put("error", "Error inesperado");
+        error.put("mensaje", ex.getMessage());
 
         return Mono.just(error);
     }

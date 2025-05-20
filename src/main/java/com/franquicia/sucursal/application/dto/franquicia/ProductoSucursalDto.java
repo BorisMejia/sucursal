5150f3560274736b0fccf6ece2df70f32fab8a35
BorisMejia <borisfernandez916@gmail.com>
2025-05-20 12:10:36 -0500

    delete: se elimina dto no utilizado

--- src/main/java/com/franquicia/sucursal/application/dto/franquicia/ProductoSucursalDto.java
+++
@@ -1,5 +0,0 @@
-package com.franquicia.sucursal.application.dto.franquicia;
-
-import com.franquicia.sucursal.domain.model.Producto;
-
-public record ProductoSucursalDto(String idProducto, Producto producto) {}

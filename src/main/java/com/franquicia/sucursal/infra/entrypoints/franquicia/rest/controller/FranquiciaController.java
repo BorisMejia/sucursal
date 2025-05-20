be71197f0027c3bfe66788f5f42bb28c76f6adc1
BorisMejia <borisfernandez916@gmail.com>
2025-05-20 11:15:39 -0500

    feat: se implementan los demás metodos http

---
+++ src/main/java/com/franquicia/sucursal/infra/entrypoints/franquicia/rest/controller/FranquiciaController.java
@@ -0,0 +1,106 @@
+package com.franquicia.sucursal.infra.entrypoints.franquicia.rest.controller;
+
+import com.franquicia.sucursal.application.dto.franquicia.mapper.FranquiciaDtoMapper;
+import com.franquicia.sucursal.application.dto.franquicia.request.FranquiciaRequestDto;
+import com.franquicia.sucursal.application.dto.franquicia.request.ProductoRequestDto;
+import com.franquicia.sucursal.application.dto.franquicia.request.SucursalRequestDto;
+import com.franquicia.sucursal.application.dto.franquicia.response.FranquiciaResponseDto;
+import com.franquicia.sucursal.application.usecase.franquicia.FranquiciaService;
+import com.franquicia.sucursal.domain.model.Franquicia;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(value = "/franquicias", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class FranquiciaController {
+
+    private final FranquiciaService franquiciaService;
+    private final FranquiciaDtoMapper mapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<FranquiciaResponseDto> crearFranquicia(@Valid @RequestBody FranquiciaRequestDto dto) {
+        return franquiciaService.crearFranquicia(dto.id(), dto.nombre())
+                .map(mapper::toDto);
+    }
+
+    @GetMapping
+    public Flux<FranquiciaResponseDto> listarFranquicias() {
+        return franquiciaService.obtenerTodasLasFranquicias()
+                .map(mapper::toDto);
+    }
+
+    @PostMapping("/{idFranquicia}/sucursales")
+    public Mono<FranquiciaResponseDto> agregarSucursal(
+            @PathVariable String idFranquicia,
+            @Valid @RequestBody SucursalRequestDto dto) {
+        return franquiciaService.agregarSucursal(idFranquicia, dto.id(), dto.nombre())
+                .map(mapper::toDto);
+    }
+
+    @PutMapping("/{idFranquicia}/nombre")
+    public Mono<FranquiciaResponseDto> actualizarNombreFranquicia(
+            @PathVariable String idFranquicia,
+            @RequestBody FranquiciaRequestDto dto) {
+        return franquiciaService.actualizarNombreFranquicia(idFranquicia, dto.nombre())
+                .map(mapper::toDto);
+    }
+
+    @PutMapping("/{idFranquicia}/sucursales/{idSucursal}/nombre")
+    public Mono<FranquiciaResponseDto> actualizarNombreSucursal(
+            @PathVariable String idFranquicia,
+            @PathVariable String idSucursal,
+            @RequestBody SucursalRequestDto dto) {
+        return franquiciaService.actualizarNombreSucursal(idFranquicia, idSucursal, dto.nombre())
+                .map(mapper::toDto);
+    }
+
+    @PostMapping("/{idFranquicia}/sucursales/{idSucursal}/productos")
+    public Mono<FranquiciaResponseDto> agregarProducto(
+            @PathVariable String idFranquicia,
+            @PathVariable String idSucursal,
+            @Valid @RequestBody ProductoRequestDto dto) {
+        return franquiciaService.agregarProducto(idFranquicia, idSucursal, dto.id(), dto.nombreProducto(), dto.stock())
+                .map(mapper::toDto);
+    }
+
+    @PutMapping("/{idFranquicia}/sucursales/{idSucursal}/productos/{idProducto}/nombre")
+    public Mono<FranquiciaResponseDto> actualizarNombreProducto(
+            @PathVariable String idFranquicia,
+            @PathVariable String idSucursal,
+            @PathVariable String idProducto,
+            @RequestBody ProductoRequestDto dto) {
+        return franquiciaService.actualizarNombreProducto(idFranquicia, idSucursal, idProducto, dto.nombreProducto())
+                .map(mapper::toDto);
+    }
+
+    @PutMapping("/{idFranquicia}/sucursales/{idSucursal}/productos/{idProducto}/stock")
+    public Mono<FranquiciaResponseDto> actualizarStockProducto(
+            @PathVariable String idFranquicia,
+            @PathVariable String idSucursal,
+            @PathVariable String idProducto,
+            @RequestBody ProductoRequestDto dto) {
+        return franquiciaService.actualizarStockProducto(idFranquicia, idSucursal, idProducto, dto.stock())
+                .map(mapper::toDto);
+    }
+
+    @DeleteMapping("/{idFranquicia}/sucursales/{idSucursal}/productos/{idProducto}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> eliminarProducto(
+            @PathVariable String idFranquicia,
+            @PathVariable String idSucursal,
+            @PathVariable String idProducto) {
+        return franquiciaService.eliminarProducto(idFranquicia, idSucursal, idProducto).then();
+    }
+
+    @GetMapping("/{idFranquicia}/productos-mayor-stock")
+    public Flux<String> obtenerProductosMayorStockPorSucursal(@PathVariable String idFranquicia) {
+        return franquiciaService.obtenerProductosConMayorStockPorSucursal(idFranquicia);
+    }
+}
